e498f1f9e584ab93a81fa8a8e3dc6b895a28eaba
Shane Vitarana <shanev@gmail.com>
2019-11-02 16:57:56 -0700

    Removed ibc

--- x/ibc/02-client/types/tendermint/doc.go
+++
@@ -1,5 +0,0 @@
-/*
-Package tendermint implements a concrete `ConsensusState`, `Header` and `Equivocation`
-for the Tendermint consensus algorithm.
-*/
-package tendermint
--- x/ibc/20-transfer/keeper/relay_test.go
+++
@@ -1,6 +0,0 @@
-package keeper_test
-
-func (suite *KeeperTestSuite) TestSendTransfer() {
-	err := suite.keeper.SendTransfer(suite.ctx, "", "", nil, nil, nil, true)
-	suite.Error(err)
-}
