d22f5d7248bc690ce1d50e877f65256ef4d715a7
Shane Vitarana <shanev@gmail.com>
2019-11-02 21:42:11 -0700

    Filled in codec

--- x/redaomint/codec.go
+++ x/redaomint/codec.go
@@ -1,7 +1,7 @@
 package redaomint
 
 import (
-"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec"
 )
 
 // RegisterCodec registers the account types and interface
@@ -9,6 +9,10 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgCreateReDAOMint{}, "redaomint/MsgCreateReDAOMint", nil)
 	cdc.RegisterConcrete(MsgMintShares{}, "redaomint/MsgMintShares", nil)
 	cdc.RegisterConcrete(MsgAllocateLandShares{}, "redaomint/MsgAllocateLandShares", nil)
+	cdc.RegisterConcrete(MsgDistributeCredit{}, "redaomint/MsgDistributeCredit", nil)
+	cdc.RegisterConcrete(MsgPropose{}, "redaomint/MsgPropose", nil)
+	cdc.RegisterConcrete(MsgVote{}, "redaomint/MsgVote", nil)
+	cdc.RegisterConcrete(MsgExecProposal{}, "redaomint/MsgExecProposal", nil)
 }
 
 // ModuleCdc generic sealed codec to be used throughout module
